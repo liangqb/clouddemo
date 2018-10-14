ee33c54184df936f7caa0a2ff8444df341d33cd1
Lqb <281210097@qq.com>
2018-10-14 20:46:13 +0800

    update by eclipse

--- ws/src/main/java/ws/ws/service/impl/UserServiceImpl.java
+++ ws/src/main/java/ws/ws/service/impl/UserServiceImpl.java
@@ -18,7 +18,9 @@ public class UserServiceImpl implements UserService {
     public List<Category> findAll() {
         System.out.println("clouddemo2");
         System.out.println("clouddemo1");
-        System.out.println("clouddemo8");
+        System.out.println("clouddemo4");
+        System.out.println("clouddemo6");
+        System.out.println("clouddemo8");        
         System.out.println("clouddemo9");
         return userMapper.findAll();
     }
